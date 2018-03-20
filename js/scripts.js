acb6177d68fb4378120c7e95d06aac64d06f00af
OrbitGirl21 <35708935+OrbitGirl21@users.noreply.github.com>
2018-03-20 14:09:02 -0700

    Ver 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+console.log('Be like Browser, hello');
+
+// Light Gallery
+$('#gallery').lightGallery();
+
+// Randomizer
+var max = 3;
+var randNum = Math.floor( Math.random() * max );
+
+// Greetings
+var msg0 = 'Come have a';
+var msg1 = 'Have a';
+var msg2 = 'Give me a';
+
+// Change Greeting
+
+$('h2 span').text( eval('msg' + randNum) );
+
+console.log( randNum );
+
+$('.parallax-window').parallax({imageSrc: 'img/Tequila_lime.jpg'});
